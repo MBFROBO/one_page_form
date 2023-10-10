174b47e33aab8c2838a398c0dfd7da9e096e122e
mbfrobo <khramtsov628@gmail.com>
2023-10-10 15:00:38 +0300

    feach: запросы на сервис фарватера

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,44 @@
-const APP = '';
+const APP = 'dispatcher-room-ui';
 const URLS = [
     'https://dispatcher.farvater.group/api/auth/',
-    'https://dispatcher.farvater.group/api/autz/'
+    'https://dispatcher.farvater.group/api/autz/',
+    'https://dispatcher.farvater.group/api/ability/',
+    'https://dispatcher.farvater.group/'
 ];
 
-function autz_module(auth_token) {
-    axios.post(URLS[1]).then((response) => {
 
-    });
-}
 
-function auth_module(username, password) {
-    axios.post(URLS[0]).then((response) => {
+function auth_module(username, password, query) {
+    axios.post(URLS[0], query.auth_schema(username,password)).then((response) => {
+        let rsp = response;
+        // var form = new FormData();
+        // form.append('username', username);
+        // form.append('password',password);
+        var auth_token = rsp['data']['data']['login']['token'];
+        var auth_refresh_token = rsp['data']['data']['login']['refreshToken'];
 
+        axios.post(URLS[1],{'query':query.autz_query()}, {headers: query.autz_schema(auth_token, APP)}).then((response) => {
+            if (response['data']['data']["authorize"]['token']) {
+                var autz_token = response['data']['data']["authorize"]['token'];
+                var autz_refresh_token = response['data']['data']["authorize"]['refreshToken'];
+
+                document.cookie = 'domain=https://dispatcher.farvater.group/';
+                document.cookie = 'AXIIT_FARVATER-authentication-token='+auth_token;
+                document.cookie = 'AXIIT_FARVATER-authentication-refresh-token='+auth_refresh_token;
+                document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-token=' + autz_token;
+                document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-refresh-token='+autz_refresh_token
+
+                axios.post(URLS[2], {'query':query.ability_query()}, {headers: {
+                    'Authorization':'Bearer '+autz_token,
+                    'Access-Control-Allow-Origin':'dispatcher.farvater.group'
+                }, withCredentials:true}).then((response) => {
+                    if (response.status == 200) {
+                        window.location.href = URLS[3];
+
+                    }
+                })              
+            }
+        });
     });
 }
 
---
+++ static/js/query.js
@@ -0,0 +1,91 @@
+
+class Query {
+    static auth_query() {
+        let query = 'mutation loginPortal($login3: String!, $pass3: String!){' +
+            'login(login: $login3, password: $pass3){' +
+              '...AuthFragment __typename' +
+              '}'+
+          '}'+
+          'fragment AuthFragment on AuthPayload {'+
+            'token refreshToken '+
+            'account {' +
+                'id login email phone name '+
+                 'banned ' +
+                 'createdAt updatedAt'+  
+               '}'+ 
+          '}';
+        return query
+    }
+
+    static autz_query() {
+        let query = 'mutation Autz{' +
+            'authorize { token refreshToken }' +
+        '}';
+        return query
+    }
+
+    static auth_schema(login, passw) {
+        let json = {
+            "login3":login,
+            "pass3":passw
+        };
+        
+        let json_request = {
+            "query":Query.auth_query(),
+            "variables":json,
+            "operationName":"loginPortal" 
+        };
+
+        return json_request
+    }
+    static autz_schema(token, app) {
+        let headers = {
+            "authentication":'Bearer '+ token,
+            "application":app
+        }
+        return headers
+    }
+
+    static ability_query() {
+        let query = "query getAbility {" +
+            "data: getAbility {" +
+              "profile {" +
+                "id" +
+              "}" +
+              "application {" +
+                "id" +
+              "}" +
+              "role {" +
+                "id}" +
+              "rules {" +
+                "action \n" +
+                "subject \n" +
+                "fields \n" +
+                "conditions \n" +
+                "inverted \n" +
+                "reason \n" +
+              "}"+
+            "}"+
+        "}"
+        return query
+    }
+
+    
+    static ability_schema() {
+        let json_request = {
+            'query':Query.ability_query(),
+            'operationName':"getAbility"
+        }
+        return json_request
+    }
+}
+
+class TokenParse {
+    static auth_token_parse(response) {
+
+    }
+}
+
+function auth_by_form() {
+    
+}
