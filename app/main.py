0d49aba31d12179cf56857655da0fa4a5699dba5
mbfrobo <khramtsov628@gmail.com>
2023-11-20 11:13:17 +0300

    feach: доабвлен бекенд на fastapi feach: добавлена админка

---
+++ app/main.py
@@ -0,0 +1,105 @@
+import uvicorn, yaml, os, aiofiles
+
+from fastapi import FastAPI, File, UploadFile, Cookie, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse, Response, HTMLResponse
+from fastapi.requests import Request
+
+from typing import Annotated
+from pathlib import Path
+
+app = FastAPI()
+
+app.mount('/static', StaticFiles(directory= Path(__file__).parent.absolute() / 'static', html=True),
+          name='static')
+
+templates = Jinja2Templates(directory=Path(__file__).parent.absolute() / "temp")
+@app.get('/')
+async def redirect():
+    return RedirectResponse(url='/auth')
+
+@app.get("/auth")
+async def root(request:Request):
+    try:
+        client = request.headers['client']
+    except:
+        return {'message':"Неверные заголовки запроса. Установите заголовк 'client'"}
+    
+    with open('app/db/db.yaml', 'r') as db:
+        clients = yaml.safe_load(db)
+    try: 
+        template = clients['clients'][client]['template_url']
+        return templates.TemplateResponse(template, {'request':request})
+    except KeyError:
+        return {'message':'Вы не зарегистрированы в системе брендирования'}
+    
+@app.get("/admin")
+async def admin(request:Request):
+    return templates.TemplateResponse('admin.html', {'request':request})
+
+@app.get('/admin/add_client')
+async def add_client(request:Request, auth_farvater_admin_brend:str = Cookie(None)):
+    if auth_farvater_admin_brend is not None:
+        return templates.TemplateResponse('admin_upload.html', {'request':request})
+    else:
+        return {'message':'Пользователь не авторизован на системе мониторинга'}
+    
+    
+@app.get('/getCLient')
+async def getCLient(request:Request):
+    with open('app/db/db.yaml', 'r') as db:
+        clients = yaml.safe_load(db)
+    return clients
+
+@app.delete('/deleteClient')
+async def deleteClient(request:Request):
+    """
+        Удаляем клиента
+    """
+    with open('app/db/db.yaml', 'r') as db:
+        clients = yaml.safe_load(db)
+    try:
+        
+        client = request.headers['client']
+        file_path = clients['clients'][client]['template_url']
+        
+        del clients['clients'][client]
+        
+    except KeyError:
+        return {'error': 'Invalid client'}
+    
+    with open('app/db/db.yaml', 'w') as db:
+        yaml.dump(clients, db)
+
+    os.remove(Path(__file__).parent.absolute() / f"temp/{file_path}")
+
+
+
+@app.post("/admin/add_client")
+async def create_upload_files(files: list[UploadFile], request: Request, client: Annotated[str, Form()]):
+    """
+        Загружаем файлики, хехех
+    """
+    try:
+        with open('app/db/db.yaml', 'r') as db:
+            clients = yaml.safe_load(db)
+            
+        for file in files:
+            template_file_path = Path(__file__).parent.absolute() / f"temp/{file.filename}"
+            async with aiofiles.open(template_file_path, 'wb') as out_file:
+                content = await file.read()
+                await out_file.write(content)
+            clients['clients'][client] = {'template_url': file.filename}
+
+            with open('app/db/db.yaml', 'w') as db:
+                yaml.dump(clients, db)
+                
+        return templates.TemplateResponse('admin_upload.html', {'request':request, 
+                                                            'result':f'Загрузка успешно выполнена {[file.filename for file in files]}'})
+    except IsADirectoryError:
+        return templates.TemplateResponse('admin_upload.html', {'request':request, 
+                                                            'result':f'Нужно выбрать файл!'})
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host='localhost', port=8000)
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -8,7 +8,7 @@ const URLS = [
 
 
 
-function auth_module(username, password, query) {
+function auth_module(username, password, query, admin) {
     axios.post(URLS[0], query.auth_schema(username,password)).then((response) => {
         let rsp = response;
         // var form = new FormData();
@@ -23,20 +23,31 @@ function auth_module(username, password, query) {
                 var autz_refresh_token = response['data']['data']["authorize"]['refreshToken'];
 
                 document.cookie = 'domain=https://dispatcher.farvater.group/';
+                document.cookie = 'auth_farvater_admin_brend='+auth_token;
                 document.cookie = 'AXIIT_FARVATER-authentication-token='+auth_token;
                 document.cookie = 'AXIIT_FARVATER-authentication-refresh-token='+auth_refresh_token;
                 document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-token=' + autz_token;
                 document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-refresh-token='+autz_refresh_token
 
-                axios.post(URLS[2], {'query':query.ability_query()}, {headers: {
-                    'Authorization':'Bearer '+autz_token,
-                    'Access-Control-Allow-Origin':'dispatcher.farvater.group'
-                }, withCredentials:true}).then((response) => {
-                    if (response.status == 200) {
-                        window.location.href = URLS[3];
-
+                if (admin == false) {
+                    try {
+                        axios.post(URLS[2], {'query':query.ability_query()}, {headers: {
+                            'Authorization':'Bearer '+autz_token,
+                            'Access-Control-Allow-Origin':'dispatcher.farvater.group'
+                        }, withCredentials:true}).then((response) => {
+                            if (response.status == 200) {
+                                window.location.href = URLS[3];
+        
+                            }
+                        })     
+                    } catch (e) {
+                        console.log(e);
                     }
-                })              
+                } else {
+                    window.location.href = '/admin/add_client';
+                }
+                
+                         
             }
         });
     });
