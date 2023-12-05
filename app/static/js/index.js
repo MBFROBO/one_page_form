0f09cd9bb3f9ecf6687aae5b742a1d99cc7bcce2
mbfrobo <khramtsov628@gmail.com>
2023-12-06 00:37:19 +0300

    fix

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -22,21 +22,24 @@ function auth_module(username, password, query, admin) {
             let rsp = response;
             var auth_token = rsp['data']['data']['login']['token'];
             var auth_refresh_token = rsp['data']['data']['login']['refreshToken'];
-    
+
+            document.cookie = 'domain=.farvater.group';
+            document.cookie = 'auth_farvater_admin_brend='+auth_token;
+            document.cookie = 'AXIIT_FARVATER-authentication-token='+auth_token;
+            document.cookie = 'AXIIT_FARVATER-authentication-refresh-token='+auth_refresh_token;
+
             axios.post(URLS[1],{'query':query.autz_query()}, {headers: query.autz_schema(auth_token, APP)}).then((response) => {
                 if (response['data']['data']["authorize"]['token']) {
                     var autz_token = response['data']['data']["authorize"]['token'];
                     var autz_refresh_token = response['data']['data']["authorize"]['refreshToken'];
     
                     document.cookie = 'domain=.farvater.group';
-                    document.cookie = 'auth_farvater_admin_brend='+auth_token;
-                    document.cookie = 'AXIIT_FARVATER-authentication-token='+auth_token;
-                    document.cookie = 'AXIIT_FARVATER-authentication-refresh-token='+auth_refresh_token;
+                    
                     document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-token=' + autz_token;
                     document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-refresh-token='+autz_refresh_token
 
                     if (admin == false) {
-                    axios.post('https://dispatcher.farvater.group/', query.ability_schema(), { 
+                    axios.post('https://dispatcher.farvater.group/api/ability/', query.ability_schema(), { 
                         headers: {'Authorization':'Bearer ' + autz_token},
                         withCredentials: true}).then((response) => {
                         if (response.status == 200) {
