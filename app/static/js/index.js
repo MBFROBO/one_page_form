ad5eb1675e78dba1a5e2af703d122db01028a174
mbfrobo <khramtsov628@gmail.com>
2023-12-05 20:30:55 +0300

    fix: index.js pose request

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -6,48 +6,50 @@ const URLS = [
     'https://dispatcher.farvater.group/'
 ];
 
-
-
-function auth_module(username, password, query, admin) {
+function auth_module_test(username, password, query, admin) {
     axios.post(URLS[0], query.auth_schema(username,password)).then((response) => {
         let rsp = response;
+        console.log(rsp);
         var auth_token = rsp['data']['data']['login']['token'];
         var auth_refresh_token = rsp['data']['data']['login']['refreshToken'];
-
-        axios.post(URLS[1],{'query':query.autz_query()}, {headers: query.autz_schema(auth_token, APP)}).then((response) => {
-            if (response['data']['data']["authorize"]['token']) {
-                var autz_token = response['data']['data']["authorize"]['token'];
-                var autz_refresh_token = response['data']['data']["authorize"]['refreshToken'];
-
-                document.cookie = 'domain=https://dispatcher.farvater.group/';
-                document.cookie = 'auth_farvater_admin_brend='+auth_token;
-                document.cookie = 'AXIIT_FARVATER-authentication-token='+auth_token;
-                document.cookie = 'AXIIT_FARVATER-authentication-refresh-token='+auth_refresh_token;
-                document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-token=' + autz_token;
-                document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-refresh-token='+autz_refresh_token
-
-                if (admin == false) {
-                    try {
-                        axios.post(URLS[2], {'query':query.ability_query()}, {headers: {
-                            'Authorization':'Bearer '+autz_token,
-                            'Access-Control-Allow-Origin':'dispatcher.farvater.group'
-                        }, withCredentials:true}).then((response) => {
-                            if (response.status == 200) {
-                                window.location.href = URLS[3];
         
-                            }
-                        })     
-                    } catch (e) {
-                        console.log(e);
+    })
+}
+
+function auth_module(username, password, query, admin) {
+    try {
+        axios.post(URLS[0], query.auth_schema(username,password)).then((response) => {
+            let rsp = response;
+            var auth_token = rsp['data']['data']['login']['token'];
+            var auth_refresh_token = rsp['data']['data']['login']['refreshToken'];
+    
+            axios.post(URLS[1],{'query':query.autz_query()}, {headers: query.autz_schema(auth_token, APP)}).then((response) => {
+                if (response['data']['data']["authorize"]['token']) {
+                    var autz_token = response['data']['data']["authorize"]['token'];
+                    var autz_refresh_token = response['data']['data']["authorize"]['refreshToken'];
+    
+                    // document.cookie = 'domain=https://dispatcher.farvater.group/';
+                    document.cookie = 'auth_farvater_admin_brend='+auth_token;
+                    document.cookie = 'AXIIT_FARVATER-authentication-token='+auth_token;
+                    document.cookie = 'AXIIT_FARVATER-authentication-refresh-token='+auth_refresh_token;
+                    document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-token=' + autz_token;
+                    document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-refresh-token='+autz_refresh_token
+    
+                    if (admin == false) {
+                        window.location.href = URLS[3];
+                        }
+                      else {
+                        window.location.href = '/admin/add_client';
                     }
-                } else {
-                    window.location.href = '/admin/add_client';
+                    
+                             
                 }
-                
-                         
-            }
+            });
         });
-    });
+    } catch (e) {
+        console.log(e);
+    }
+    
 }
 
 function token_parse(responce_body) {}
