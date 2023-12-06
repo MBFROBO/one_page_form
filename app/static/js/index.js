24911500e6755053083efdd4339e7cc3d07bcfc8
mbfrobo <khramtsov628@gmail.com>
2023-12-06 22:24:35 +0300

    fix: method

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -27,7 +27,7 @@ function auth_module(username, password, query, admin) {
             document.cookie = 'AXIIT_FARVATER-authentication-token='+auth_token + ';domain=.farvater.group; path=/';
             document.cookie = 'AXIIT_FARVATER-authentication-refresh-token='+auth_refresh_token + ';domain=.farvater.group; path=/';
 
-            axios.post(URLS[1],{'query':query.autz_query()}, {headers: query.autz_schema(auth_token, APP)}).then((response) => {
+            axios.post(URLS[1],{'query':query.autz_query()}, {headers: query.autz_schema(auth_token, APP)}, withCredentials=true).then((response) => {
                 if (response['data']['data']["authorize"]['token']) {
                     var autz_token = response['data']['data']["authorize"]['token'];
                     var autz_refresh_token = response['data']['data']["authorize"]['refreshToken'];
@@ -35,14 +35,13 @@ function auth_module(username, password, query, admin) {
                     document.cookie = 'AXIIT_FARVATER-dispatcher-room-ui-authorization-refresh-token='+autz_refresh_token + ';domain=.farvater.group; path=/';
 
                     if (admin == false) {
-                    axios.post('https://dispatcher.farvater.group/api/ability/', query.ability_schema(), { 
-                        headers: {'Authorization':'Bearer ' + autz_token},
-                        withCredentials: true}).then((response) => {
-                        if (response.status == 200) {
-                                window.location.href = URLS[3];    
-                            }
-                        }
-                    )} else {
+                    // axios.post('https://dispatcher.farvater.group/api/ability/', query.ability_schema(), { 
+                    //     headers: {'Authorization':'Bearer ' + autz_token},
+                    //     withCredentials: true}).then((response) => {
+                    //     if (response.status == 200) {
+                        window.location.href = URLS[3];    
+  
+                    } else {
                         window.location.href = '/admin/add_client';
                     }
                 }
